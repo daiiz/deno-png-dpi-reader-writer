97756160141522672cfdf803ada69e0af66c98e4
daiiz <da.iz.mg@gmail.com>
2019-01-19 10:33:00 +0900

    examples

---
+++ examples/reader.ts
@@ -0,0 +1,26 @@
+import {open, copy, args, Buffer} from 'deno'
+import {parsePngFormat} from '../mod.ts'
+
+console.log(args)
+
+const loadImage = async srcPath => {
+  const imgFile = await open(srcPath)
+  const buf = new Buffer()
+  await copy(buf, imgFile)
+  return buf // Buffer {off: 0, buf: []}
+}
+
+const main = async () => {
+  if (!args[1]) throw new Error('image url is required.')
+
+  const srcUrl = args[1]
+  let buf: Buffer
+  if () {
+
+  }
+  buf = await loadImage('./examples/retina/button.png')
+  console.log(parsePngFormat(buf))
+}
+
+main()
+
